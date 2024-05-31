5d593ecf1ec4c3e6f33c5e09e1b779d031da09ab
uzair rana <you@example.com>
2024-06-01 01:03:05 +0500

    chore: Refactor errorMiddleware.js for consistent variable naming and formatting

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,9 +1,8 @@
-export const errorHandler = (err,req,res,next)=>{
-    const statuscode = res.statuscode === 200 ? 500 : res.statuscode;
-    res.statuscode(statuscode);
+export const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
     res.json({
-        message : err.message,
-        stack : process.env.NODE_ENV === 'production' ?null :err.stack 
-        
-    })
-}
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    });
+};
--- backend/server.js
+++ backend/server.js
@@ -4,31 +4,20 @@ import dotenv from 'dotenv';
 import  { connectDB } from './confiq/db.js'
 import userRouter from "./Routes/UserRoutes.js";
 import { errorHandler } from './middlewares/errorMiddleware.js';
-
-
 dotenv.config();
-
-
-
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 connectDB();
 app.get("/",(req,res)=>{
     console.log("API is rinning ")
     res.send("api is running ")
 })
-
-
- app.use("/api/users",userRouter);
-
- app.use(errorHandler);
-
+ app.use("/api/users",userRouter,()=>{
+    res.send("api is hunging ")
+ });
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
-
-
 app.listen(PORT,()=>{
     console.log(`server is running on http:localhost:${PORT}`)
 })
